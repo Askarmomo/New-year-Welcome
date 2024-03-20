84ad74ff4922bf168177b55a1f950ed4b1091785
Askarmomo <Askarmomo70@gmail.com>
2024-03-20 19:57:34 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html","./greeting.html",
+    "./index.html",
+    "./greeting.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./assets/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
